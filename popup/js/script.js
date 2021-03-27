0e234d4ccb33c8d450b06330d83b9bee6aedcdab
GeethuEipe <geethueipe97@gmail.com>
2021-03-27 18:09:43 +0530

    first commit

---
+++ popup/js/script.js
@@ -0,0 +1,133 @@
+/*alert("Alert");
+confirm("Confirm");
+prompt("prompt");*/
+
+/*Accessing values from prompt
+
+var a=prompt("Enter your name");
+document.write(a);*/
+
+/*var x=parseInt(prompt("Enter a first no"));
+var y=parseInt(prompt("Enter a second no"));
+var result=x+y;
+alert(result);*/
+
+/*var a=prompt("Enter a word");
+document.write(a);*/
+/*document.getElementById("demo").innerHTML="orisys";*/
+
+
+/* var a=10;
+ document.write("<h1>"+a);
+ 
+ function myfun()
+ {
+	 var a=5;
+	 document.write("<h1>"+a);
+ }
+ myfun();
+ document.write("<h1>"+a);*/
+ /*document.getElementById("demo").innerHTML="orisys";*/
+ 
+ 
+/* function change()
+ {
+	 var x=document.getElementById("demo").value;
+	 alert(x);
+ }*/
+ 
+ 
+ 
+ /*function change()
+ {
+	 var x=document.getElementById("demo1").value;
+	 var y=document.getElementById("demo2").value;
+	 
+	 alert(x+y);
+ }*/
+ /*Array Section*/
+/*var fruit=['mango','apple','orange'];
+ document.write("<h1>"+fruit+"</h1>");
+ document.write("<h1>"+fruit[1]+"</h1>");
+ fruit.push('pineapple');
+ document.write("<h1>"+fruit+"</h1>");
+ fruit.pop();
+ document.write("<h1>"+fruit+"</h1>");
+ fruit.unshift('pineapple');
+ document.write("<h1>"+fruit+"</h1>");
+fruit.shift('pineapple');
+ document.write("<h1>"+fruit+"</h1>");*/
+ /*var fruit=['mango','apple','orange'];
+ for(var i=0;i<fruit.length;i++)
+ {
+	 document.write("<h1>"+fruit[i]+"</h1>");
+ }*/
+ 
+ 
+ /*function change()
+ {
+ var rd1=document.getElementById("rd1");
+ var rd2=document.getElementById("rd2");
+	 if(rd1.checked==true)
+	 {
+		 alert("selected"+rd1.value);
+	 }
+	 else if(rd2.checked==true)
+	 {
+		  alert("selected"+rd2.value);
+	 }
+	 else
+	 {
+		 alert("error")
+	 }
+ }*/
+ 
+	
+
+
+/*function change()
+{
+	var select=document.getElementById("selectbox");
+	alert(select.options[select.selectedIndex].value);
+}*/
+
+/*function change()
+{
+
+var para=document.getElementsByTagName("p");
+for(var i=0;i<para.length;i++)
+{
+
+	para[i].style.color="red";
+
+}
+}*/
+/*function change()
+{
+	var para=document.getElementsByClassName("demo");
+	for(var i=0;i<para.length;i++)
+		{
+
+	para[i].style.color="red";
+
+		}
+}*/
+
+
+function mouseover()
+{
+	document.getElementById("img1").src="img/2.jpg";
+	
+}
+function mouseout()
+{
+	document.getElementById("img1").src="img/3.jpg";
+}
+	
+ 
+ 
+ 
+ 
+ 
+ 
+ 
